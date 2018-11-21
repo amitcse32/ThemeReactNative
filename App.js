2b037834601b7be1c1c6f5e4708559516f415b5d
Amit Sharma <amitcse32@gmail.com>
2018-11-21 15:13:55 +0530

    New Test 1

--- App.js
+++ App.js
@@ -6,8 +6,8 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native Amit Sharma NEW AMIT!</Text>
+        <Text style={styles.welcome}>Welcome to React Native Amit Sharma NEW AMIT SHARMA NEW ONE!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
